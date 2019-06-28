1bbe901d2ffd1f685f01163ac2c4d388e38c691b
VarunBhandia <varunbhandia@gmail.com>
2019-06-28 11:59:46 +0530

    Minor cahnges

--- project/server.js
+++ project/server.js
@@ -42,7 +42,7 @@ app.get('/api/getAll', (req, res) => {
   });
 });
 
-const server = app.listen(3000, () => {
+const server = app.listen(3001, () => {
 
   const host = server.address().address;
   const port = server.address().port;
