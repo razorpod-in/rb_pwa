044c5ee24ff84739c36ff0fd50725399efa38233
VarunBhandia <varunbhandia@gmail.com>
2019-07-12 12:54:33 +0530

    Added user last chapter and module track

--- project/app/js/main.js
+++ project/app/js/main.js
@@ -1,203 +1,112 @@
-/*
-Copyright 2018 Google Inc.
+  // Global variables
+  var user_type = '';
+  var selected = "none"
+  var timePeriodInMs = 2000;
+  var user_info_status = 0;
+  var user_name = '';
+  var user_number = '';
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-// TODO - register service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(registration => {
-        // console.log(`Service Worker registered! Scope: ${registration.scope}`);
-      })
-      .catch(err => {
-        // console.log(`Service Worker registration failed: ${err}`);
-      });
-  });
-}
-
-// Container
-const container = document.getElementById('module-card-container');
-const chapterContainer = document.getElementById('chapter-card-container');
-
-Notification.requestPermission();
-
-// TODO - create indexedDB database
-const dbPromise = createIndexedDB();
-
-// API calls to fetch the modules
-const getModules = async () => {
-  try {
-    var reponseModules = await axios.get('/server/modules');
-    return reponseModules.data.payload;
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-const getChapter = async (id) => {
-  try {
-    var url = "/server/chapters/"+id;
-    var reponseChapter = await axios.get(url);
-    return reponseChapter.data.payload;
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-loadContentNetworkFirst();
-
-function saveEventDataLocally(modules) {
-  if (!('indexedDB' in window)) {
-    return null;
-  }
-  return dbPromise.then(db => {
-    
-    const tx = db.transaction('modules', 'readwrite');
-    const store = tx.objectStore('modules');
-    return Promise.all(store.put(modules))
-      .catch(() => {
-        tx.abort();
-        throw Error('Modules were not added to the store');
-      });
-  });
-}
-
-function getLocalEventData() {
-  if (!('indexedDB' in window)) {
-    return null;
-  }
-  return dbPromise.then(db => {
-    const tx = db.transaction('modules', 'readonly');
-    const store = tx.objectStore('modules');
-    return store.getAll();
-  });
-}
-
-function loadContentNetworkFirst() {
-  getModules()
-    .then(dataFromNetwork => {
-      saveEventDataLocally(dataFromNetwork)
-        .then(() => {
-          setLastUpdated(new Date());
-        }).catch(err => {
-          console.warn(err);
-        });
-
-      getLocalEventData()
-        .then(offlineData => {
-          if (!offlineData.length) {} else {
-            updateUI(offlineData);
-          }
-        });
-    }).catch(err => {
-      getLocalEventData()
-        .then(offlineData => {
-          if (!offlineData.length) {} else {
-            updateUI(offlineData);
-          }
-        });
-    });
-}
-
-function createIndexedDB() {
-  if (!('indexedDB' in window)) {
-    return null;
-  }
-  return idb.open('dashboardr', 2, function (upgradeDb) {
-    if (!upgradeDb.objectStoreNames.contains('modules')) {
-      const modulesOS = upgradeDb.createObjectStore('modules', {
-        keyPath: 'id'
-      });
-    }
-  });
-}
-/* Network functions */
-
-function getServerData() {
-  return fetch('api/getAll').then(response => {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  });
-}
-
-
-
-/* UI functions */
-
-function updateUI(modules) {
-  modules.forEach(module => {
-    var card = `
-      <a href="chapter.html?id=${module.id}">
-        <div class="module-card">
-        
-            <div class="row">
-                    <div class="col-xs-6">
-                      <img class="module-card-image" src="${module.thumbnail}">
-                    </div>
-                    <div class="col-xs-6">
-                      <p class="module-card-heading">${module.module_name}</p>
-                      <p class="module-card-sub-heading">${module.module_description}</p>
-                    </div>
-                
-            </div>
-        </div>
-      </a>
-    `
-    if (container) {
-      container.insertAdjacentHTML('beforeend', card);
-    }
-  });
-}
-
-function chapterUI(modules) {
-  var url_string = window.location.href;
-  var url = new URL(url_string);
-  var c = url.searchParams.get("id");
-  var steps = ''
-  var module_name = ''
-  modules.forEach(module => {
-    if (c == module.id) {
-      module_name = module.module_name
-      steps = module.steps
-    }
-  });
-
-  if (steps) {
-    steps.forEach(step => {
-      const card = `
-        <div class="col-xs-6">
-          
-            <img src="${step.thumbnail}" onclick="wow('${step.id}','${module_name}')" style="width:100%">
-          
-          <p class="chapter-card-heading">${step.step_title}</p>
-        </div>`;
-      if (chapterContainer) {
-        chapterContainer.insertAdjacentHTML('beforeend', card);
+  // Functions 
+  setTimeout(function () {
+      if (1 == 1) {
+        document.getElementById("splash-screen").style.display = "none";
+        document.getElementById("pick-screen").style.display = "block";
+        // document.getElementById("splash-screen").style.display = "none";
+        // document.getElementById("tabs-screen").style.display = "block";
+      } else {
+        document.getElementById("splash-screen").style.display = "none";
+        document.getElementById("pick-screen").style.display = "block";
       }
-    });
+
+    },
+    timePeriodInMs);
+
+  var sound = new Howl({
+    src: ['assets/amr/intro.mp3'],
+    preload: true,
+    onend: function () {
+      $('.asha_didi').addClass('hide_didi')
+      console.log('Sound Over. Didi Hide')
+    }
+  });
+
+  function select_one(id_select) {
+    if (selected == "none") {
+      $('.asha_didi').removeClass('hide_didi')
+
+      sound.play()
+    }
+    if (id_select.id == "c1") {
+      selected = "1"
+      user_type = 'pregnant';
+      document.getElementById(id_select.id).style.borderColor = "green";
+      document.getElementById("c2").style.borderColor = "white";
+    } else {
+      selected = '2'
+      user_type = 'mother';
+      document.getElementById(id_select.id).style.borderColor = "green";
+      document.getElementById("c1").style.borderColor = "white";
+    }
+    document.getElementById("b1").classList.add('clicked');
   }
 
-}
+  function router_registration() {
+    var status = (selected == "none") ? null : 1;
+    if (status) {
+      document.getElementById("registration-screen").style.display = "block";
+      document.getElementById("pick-screen").style.display = "none";
+    }
+  }
 
-/* Storage functions */
+  function valid_form() {
+    var num_user = document.getElementById("num_id").value;
+    var name_user = document.getElementById("name_id").value;
+    if (num_user.length == 10 && name_user.length >= 4) {
+      user_info_status = 1;
+      user_name = name_user;
+      user_number = num_user;
+    } else {
+      user_info_status = 0;
+    }
 
-function getLastUpdated() {
-  return localStorage.getItem('lastUpdated');
-}
+    if (user_info_status > 0) {
+      if (document.getElementById("b2").classList.contains('clicked')) {
 
-function setLastUpdated(date) {
-  localStorage.setItem('lastUpdated', date);
-}
+      } else {
+        document.getElementById('b2').className += " clicked";
+      }
+    }
+
+    if (user_info_status == 0) {
+      document.getElementById('b2').className = "pick-screen-button";
+    }
+  }
+
+  function router_tabs_screen() {
+    if (user_info_status > 0) {
+      
+      document.getElementById("registration-screen").style.display = "none";
+      document.getElementById("tabs-screen").style.display = "block";
+    }
+  }
+
+  function open_tab(event, tabName) {
+
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tab-link");
+    var tab_id = "img-" + tabName;
+    for (i = 0; i < tablinks.length; i++) {
+      if (tablinks[i].id == tab_id) {
+        tablinks[i].src = "images/nav-icons/selected-" + tablinks[i].id + ".png"
+      } else {
+        tablinks[i].src = "images/nav-icons/" + tablinks[i].id + ".png"
+      }
+    }
+    document.getElementById(tabName).style.display = "block";
+  }
